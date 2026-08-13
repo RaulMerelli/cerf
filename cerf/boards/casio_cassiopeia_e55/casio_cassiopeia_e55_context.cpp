508485d77de22f0bcfad1449a1d7c631e65326f4
Yaroslav Kibysh <yanet.prod@gmail.com>
2026-08-13 19:51:38 +0300

    remove wrong GA window

--- cerf/boards/casio_cassiopeia_e55/casio_cassiopeia_e55_context.cpp
+++ cerf/boards/casio_cassiopeia_e55/casio_cassiopeia_e55_context.cpp
@@ -16,11 +16,6 @@ public:
     std::optional<PreferredWindowSize> GetPreferredWindowSize() const override {
         return PreferredWindowSize{ 240, 320 };
     }
-
-    /* VR4111 UM (U13137EJ2V0UM00) Table 6-6: PA 0x04000000-0x09FFFFFF is
-       "Space reserved for future use"; PA at/above 0x20000000 is a mirror of
-       0x00000000-0x1FFFFFFF. */
-    uint32_t GuestAdditionsWindowBase() const override { return 0x04000000u; }
 };
 
 }  /* namespace */
